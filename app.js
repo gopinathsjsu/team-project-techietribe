2bd2e127d981ce94ee5dbc9163d25c64d92573e1
BhavyaTetali <bhavyatetali3@gmail.com>
2021-04-11 23:23:49 -0700

    cleared merge conflicts

--- app.js
+++ app.js
@@ -9,26 +9,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var addAccountRouter = require('./routes/addAccount');
 var addTransactionRouter = require('./routes/addTransactions');
-<<<<<<< HEAD
 // var newaddAccountRouter = require('./routes/newaddAccount');
-=======
 var closeAccountRouter = require('./routes/closeAccount');
->>>>>>> 7eb8459... Close Account,Card,Account
 
 require('dotenv').config()
 var app = express();
 
 // view engine setup
-<<<<<<< HEAD
-
 app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
-=======
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
->>>>>>> 7eb8459... Close Account,Card,Account
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -40,11 +32,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/addAccount', addAccountRouter);
 app.use('/addTransaction', addTransactionRouter);
-<<<<<<< HEAD
 // app.use('/newaddTransaction', newaddAccountRouter);
-=======
 app.use('/closeAccount', closeAccountRouter);
->>>>>>> 7eb8459... Close Account,Card,Account
+
 
 // catch 404 and forward to error handler
 
