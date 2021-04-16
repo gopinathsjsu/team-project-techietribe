30a2b2bdb4719b9070be16eab2641dab63518ae1
kotgirep <pranjalikotgire57@gmail.com>
2021-04-16 14:35:55 -0700

    minor code change for transaction

--- routes/addTransactions.js
+++ routes/addTransactions.js
@@ -1,29 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var mySQL = require('mysql');
-
-var pool = mySQL.createPool({
-  connectionLimit: 1000,
-  host: 'cmpe202-project.czqzb1wsgkyi.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'Techietribe',
-  //database: 'cmpe202-project',
-});
-var high = 1000;
-var low = 1;
-var transactionId = Math.floor(Math.random() * (high - low) + low);
 const AWS = require('aws-sdk');
 
-// ADDING MANUAL TRANSCATIONS LIKE TAXES AND ALL
+function addTransactionsHelper(mySQLObj, req, res, next) {
+  var pool = mySQL.createPool({
+    connectionLimit: 1000,
+    host: 'cmpe202-project.czqzb1wsgkyi.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Techietribe',
+  });
+  var high = 1000;
+  var low = 1;
+  var transactionId = Math.floor(Math.random() * (high - low) + low);
+  // ADDING MANUAL TRANSCATIONS LIKE TAXES AND ALL
 
-function addTransaction(req, res) {
-  console.log('In beginning of adding manual transactions!!');
+  // function addTransaction(req, res) {
+  //   console.log('In beginning of adding manual transactions!!');
 
   var id = transactionId;
-  var account_id = req.query.account_id;
-  var description = req.query.description;
-  var amount = req.query.amount;
-  var date = req.query.date;
+  var account_id = req.body.account_id;
+  var description = req.body.description;
+  var amount = req.body.amount;
+  var date = req.body.date;
 
   console.log('id: ' + id);
   console.log('account_id: ' + account_id);
@@ -43,7 +42,7 @@ function addTransaction(req, res) {
         //connection.release();
         if (err) {
           console.log('Error inserting records in table' + err);
-          return res.status(500).send('failed to add new Transaction !!');
+          res.status(500).send('failed to add new Transaction !!');
         } else {
           console.log(
             'New Transaction Added Successfully with TransactionId:' + id
@@ -52,10 +51,15 @@ function addTransaction(req, res) {
             .status(200)
             .send(JSON.stringify({ message: 'success' }, null, '\t'));
         }
+        next();
       }
     );
   });
 }
+
+function addTransaction(req, res, next) {
+  addTransactionsHelper(mySQL, req, res, next);
+}
 router.post('/', addTransaction);
 module.exports = router;
-module.exports.addTransaction = addTransaction;
+module.exports.addTransactionsHelper = addTransactionsHelper;
