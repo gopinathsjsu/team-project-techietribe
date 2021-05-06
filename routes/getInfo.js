6f665dd49d3f5913c38d1fbfaeaceb97aba7aefb
BhavyaTetali <bhavyatetali3@gmail.com>
2021-05-05 21:53:32 -0700

    updated ui reference code, changed the id to customer_id

--- routes/getInfo.js
+++ routes/getInfo.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 function customerInfo(req, res, next){
-     var id = req.body.id
+     var customer_id = req.body.customer_id
      var first_name = req.body.first_name
      var last_name = req.body.last_name 
      var is_admin = req.body.is_admin 
     res.status(200).json({
-        id: id,
+        customer_id: customer_id,
         firstName : first_name,
         lastName: last_name,
         isAdmin: is_admin
